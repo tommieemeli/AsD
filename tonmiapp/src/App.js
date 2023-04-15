5a3fdb378721473dde0e0098105afa10c74082b0
Tommi Haapa <tommi-haapa@hotmail.com>
2023-04-15 15:36:12 +0300

    styling

--- tonmiapp/src/App.js
+++ tonmiapp/src/App.js
@@ -1,15 +1,40 @@
-import emote2 from "./kuvat/emote2.png"
-import './App.css';
+import emote2 from "./kuvat/emote2.png";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={emote2} className="App-logo" alt="logo" />
-        <b style={{ marginTop: 100 }}>{"Mee töihi"}</b>
+        <b className="otsikko" style={{ marginTop: 100 }}>
+          {"Mee töihi"}
+        </b>
+        <div style={{ paddingTop: "2rem" }}>
+          <a
+            href="https://www.instagram.com/taikuritommi/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/tommihaapa/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+          <a
+            href="https://github.com/tommieemeli"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
